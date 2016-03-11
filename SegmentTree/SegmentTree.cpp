9253cce2341f8449e8b5a7b604e9377a2db3a6e5
Tianyang Hu <tianyang.hu01@gmail.com>
2016-03-11 00:07:24 -0800

    Code cleanup for ST

--- SegmentTree/SegmentTree.cpp
+++ SegmentTree/SegmentTree.cpp
@@ -42,9 +42,9 @@ private:
     int sum(SegmentTreeNode* node, int start, int end) {
         if(start>node->r || end<node->l) return 0;
         if(start<=node->l && end>=node->r) return node->sum;
-        int mid = (node->r+node->l)/2;
-        if(end<=mid) return sum(node->left, start, end);
-        if(start>mid) return sum(node->right, start, end);
+        //int mid = (node->r+node->l)/2;
+        //if(end<=mid) return sum(node->left, start, end);
+        //if(start>mid) return sum(node->right, start, end);
         return sum(node->left, start, end) + sum(node->right, start, end);
     }
     
@@ -66,34 +66,30 @@ private:
     SegmentTreeNode *root; 
 };
 
+int testST(SegmentTree &st, int s, int e) {
+    printf("[%d]-[%d] = %d\n", s, e, st.sum(s, e));
+}
 int main() {
     vector<int> nums = {1,2,3,4,5,6,7,8,9};
     SegmentTree st(nums);
-    int s=0; int e=10;
-    printf("[%d]-[%d] = %d\n", s, e, st.sum(s, e));
-    s=-2; e =-1;
-    printf("[%d]-[%d] = %d\n", s, e, st.sum(s, e));
-    s=10; e=15;
-    printf("[%d]-[%d] = %d\n", s, e, st.sum(s, e));
-    s=-1; e=2;
-    printf("[%d]-[%d] = %d\n", s, e, st.sum(s, e));
-    s=1; e=2;
-    printf("[%d]-[%d] = %d\n", s, e, st.sum(s, e));
-    s=2;e=9;
-    printf("[%d]-[%d] = %d\n", s, e, st.sum(s, e));
+    testST(st, 0, 10);
+    testST(st, -2, -1);
+    testST(st, 10, 15);
+    testST(st, -1, 2);
+    testST(st, 1, 2);
+    testST(st, 2, 9);
+    testST(st, 3, 7);
+    testST(st, 5, 8);
     st.update(2, 20);
     st.update(5, 30);
-    s=0; e =10;
-    printf("[%d]-[%d] = %d\n", s, e, st.sum(s, e));
-    s=-2; e =-1;
-    printf("[%d]-[%d] = %d\n", s, e, st.sum(s, e));
-    s=10; e=15;
-    printf("[%d]-[%d] = %d\n", s, e, st.sum(s, e));
-    s=-1; e=2;
-    printf("[%d]-[%d] = %d\n", s, e, st.sum(s, e));
-    s=1; e=2;
-    printf("[%d]-[%d] = %d\n", s, e, st.sum(s, e));
-    s=2;e=9;
-    printf("[%d]-[%d] = %d\n", s, e, st.sum(s, e));
+    testST(st, 0, 10);
+    testST(st, -2, -1);
+    testST(st, 10, 15);
+    testST(st, -1, 2);
+    testST(st, 1, 2);
+    testST(st, 2, 9);
+    testST(st, 3, 7);
+    testST(st, 5, 8);
+
     return 0;
 }
